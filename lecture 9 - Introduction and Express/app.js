4fd39b8282eb8064731e99a0cc4dbc69fd3ebdcc
Dawood Shahzad <yourgithubemail@gmail.com>
2026-01-09 12:19:45 +0500

    Middelware new code add

--- lecture 9 - Introduction and Express/app.js
+++ lecture 9 - Introduction and Express/app.js
@@ -1,9 +1,23 @@
+//Core Modules
 const http = require("http");
+//External Modules
+const express = require("express")
+//Local Modules
 const requestHandler = require("./user");
 
 const PORT = 9875;
 
-const server = http.createServer(requestHandler);
+const app = express()
+ app.use((req, res, next) => {
+     console.log("Came in first Middleware", req.url, req.method);
+     next();
+ })
+
+  app.use((req, res, next) => {
+     console.log("Came in secound  Middleware", req.url, req.method)
+ })
+
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
