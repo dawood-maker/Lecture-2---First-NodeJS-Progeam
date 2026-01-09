bc4d4813c7b3230bb4f2bed77ac20303863b7e2e
Dawood Shahzad <yourgithubemail@gmail.com>
2026-01-09 14:25:31 +0500

    chang the app.js

--- lecture 9 - Introduction and Express/app.js
+++ lecture 9 - Introduction and Express/app.js
@@ -1,24 +1,27 @@
-//Core Modules
-const http = require("http");
-//External Modules
-const express = require("express")
-//Local Modules
-const requestHandler = require("./user");
+// External Modules
+const express = require("express");
+
+const app = express();
+
+/**
+ * Jab user:
+ * http://localhost:9875/submit-details
+ */
+app.get("/submit-details", (req, res) => {
+  console.log("Submit Details Route", req.url, req.method);
+  res.send("<p><h1>Came from secound Middelware</h1></p>");
+});
+
+/**
+ * Jab user:
+ * http://localhost:9875/
+ */
+app.get("/", (req, res) => {
+  console.log("Home Route", req.url, req.method);
+  res.send("<p><h2>Came from first Middelware</h2></p>");
+});
 
 const PORT = 9875;
-
-const app = express()
- app.use((req, res, next) => {
-     console.log("Came in first Middleware", req.url, req.method);
-     next();
- })
-
-  app.use((req, res, next) => {
-     console.log("Came in secound  Middleware", req.url, req.method)
- })
-
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
