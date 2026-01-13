ab0f4f09a0e8db0e4adc882ff500acfd863d3f84
Dawood Shahzad <yourgithubemail@gmail.com>
2026-01-13 21:48:38 +0500

    new coding

--- lecture 10 - airbnb project/app.js
+++ lecture 10 - airbnb project/app.js
@@ -1,3 +1,30 @@
+// const express = require("express");
+// const path = require("path");
+// const PORT = 2026;
+
+// const pagesRouter = require("./routes/pages");
+// const contactRouter = require("./routes/contact");
+
+// const app = express();
+
+// // Body parser
+// app.use(express.urlencoded({ extended: true }));
+
+// // Routes
+// app.use(pagesRouter);
+// app.use(contactRouter);
+
+// // 404 Page
+// app.use((req, res) => {
+//   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+// });
+
+// // Server
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running at  Addres http://localhost:${PORT}`);
+// });
+
+
 const express = require("express");
 const path = require("path");
 const PORT = 2026;
@@ -6,6 +33,9 @@ const pagesRouter = require("./routes/pages");
 const contactRouter = require("./routes/contact");
 
 const app = express();
+
+// ✅ STATIC FILES (THIS WAS MISSING)
+app.use(express.static(path.join(__dirname, "public")));
 
 // Body parser
 app.use(express.urlencoded({ extended: true }));
@@ -21,5 +51,5 @@ app.use((req, res) => {
 
 // Server
 app.listen(PORT, () => {
-  console.log(`✅ Server running at  Addres http://localhost:${PORT}`);
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
